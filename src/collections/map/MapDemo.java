80cd6a8a45994402d8da71773e1807ee9f1d2be0
JEDIBASIL <jedidiahbasil@gmail.com>
2023-12-05 11:17:09 +0100

    done with map

--- src/collections/map/MapDemo.java
+++ src/collections/map/MapDemo.java
@@ -1,4 +1,29 @@
 package collections.map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapDemo {
+
+    public static void main(String[] args) {
+        Map<String, String> map1 = new HashMap<>();
+
+        map1.put("name","Agrames");
+        map1.put("height","6.3");
+        map1.put("gender","male");
+
+        map1.forEach((k, v)->{
+            System.out.println(k+" = "+v);
+
+        });
+
+        System.out.println(map1.containsValue("6.3"));
+
+        System.out.println(map1.get("heights"));
+
+        map1.clear();
+        System.out.println(map1);
+
+
+    }
 }
